a3f4863be6b9f4c951c51f478e83308074c239e0
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-23 15:59:33 +0100

    create model for email

--- joshgrid/mail_api/models.py
+++ joshgrid/mail_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Mail(models.Model):
+    """
+    Define the schema for the mail to be sent by a user.
+
+    Here, we define the different parameters required when a user wants to
+    send a mail.
+    """
+    sender_address = models.CharField(max_length=100)
+    sender_name = models.CharField(max_length=100, blank=True, default='')
+    receiver_name = models.CharField(max_length=100, blank=True, default='')
+    receiver_address = models.CharField(max_length=100)
+    mail_subject = models.CharField(max_length=100)
+    mail_body = models.TextField()
+    sent = models.BooleanField(default=False)
+    delivered = models.BooleanField(default=False)
+    read = models.BooleanField(default=False)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_sent = models.DateTimeField(blank=True)
+    time_delivered = models.DateTimeField(blank=True)
+    time_read = models.DateTimeField(blank=True)
+
+
+    class Meta:
+        ordering = ('time_created',)
