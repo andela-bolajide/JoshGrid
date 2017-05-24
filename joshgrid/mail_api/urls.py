48acd466ce6c92dba91cd52fe25db1255f9d6bc0
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-24 09:37:47 +0100

    insert pattern suffix for url

--- joshgrid/mail_api/urls.py
+++ joshgrid/mail_api/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from joshgrid.mail_api import views
 
 urlpatterns = [
     url(r'^mails/$', views.mail_list),
     url(r'^mails/(?P<pk>[0-9]+)/$', views.mail_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
