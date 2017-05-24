57e667dd4dedfd57a222dbe52e550c11390d0ed8
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-24 01:10:18 +0100

    include routes for the general api

--- joshgrid/urls.py
+++ joshgrid/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^', include('joshgrid.mail_api.urls')),
     url(r'^admin/', admin.site.urls),
 ]
