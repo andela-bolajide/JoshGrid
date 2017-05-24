d34f3788cee8b639e0c287d7a8f4eb5f2ae06936
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-24 11:01:38 +0100

    switch to class-based views

--- joshgrid/mail_api/urls.py
+++ joshgrid/mail_api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from joshgrid.mail_api import views
 
 urlpatterns = [
-    url(r'^mails/$', views.mail_list),
-    url(r'^mails/(?P<pk>[0-9]+)/$', views.mail_detail),
+    url(r'^mails/$', views.MailList.as_view()),
+    url(r'^mails/(?P<pk>[0-9]+)/$', views.MailDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- joshgrid/mail_api/views.py
+++ joshgrid/mail_api/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User, Group
+from django.http import Http404
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from joshgrid.mail_api.models import Mail
 from joshgrid.mail_api.serializers import UserSerializer, GroupSerializer, MailSerializer
@@ -28,17 +26,16 @@ class GroupViewSet(viewsets.ModelViewSet):
     serializer_class = GroupSerializer
 
 
-@api_view(['GET', 'POST'])
-def mail_list(request):
+class MailList(APIView):
     """
     List all mails, or create a new mail.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         mail = Mail.objects.all()
         serializer = MailSerializer(mail, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = MailSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -46,27 +43,30 @@ def mail_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def mail_detail(request, pk):
+class MailDetail(APIView):
     """
-    Retrieve, update or delete a mail.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        mail = Mail.objects.get(pk=pk)
-    except Mail.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Mail.objects.get(pk=pk)
+        except Mail.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        mail = self.get_object(pk)
         serializer = MailSerializer(mail)
-        return JsonResponse(serializer.data, status=200)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        mail = self.get_object(pk)
         serializer = MailSerializer(mail, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        mail = self.get_object(pk)
         mail.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
