6802adf8d9418a766fb02d9dad4625b74b25e961
Bolaji Olajide <bolaji.olajide@andela.com>
2017-05-28 22:19:08 +0100

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
+const path = require('path');
+
 // webpack.config.js
 module.exports = {
-  entry: './main.js',
+  entry: './src/main.js',
   output: {
-    // path: 'build', // This is where images AND js will go
-    publicPath: 'https://joshgrid.herokuapp.com/', // This is used to generate URLs to e.g. images
+    path: path.join(__dirname,'./dist/'),
     filename: 'bundle.js'       
   },
   module: {
     loaders: [
-      { test: /(\.css)$/, 
-        loaders: ['style', 'css'] 
-      },
-      {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
+        { test: /(\.css)$/, 
+          loaders: ['style', 'css'] 
+        },
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015', 'react']
+          }
         }
-      },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
     ]
   },
   resolve: {
